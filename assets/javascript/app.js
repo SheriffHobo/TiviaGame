24b96a4bb60f46bbc914a389aba3f25721deb41e
SheriffHobo <zombiemaster@gmail.com>
2019-02-18 13:27:02 -0800

    many updates to HTML, CSS and JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,91 @@
+$(function () {
+
+// track which q they are on
+var qCounter = 0;
+// time for each question
+var time = 15;
+// tally correct guesses
+var corGuesses = 0;
+// tally incorrect guesses
+var incorGuesses = 0;
+
+// q|a array
+var questions = [
+  {
+    // 0
+    question: "What planet do the Transformers originate?",
+    choices: ["Cybertron", "Earth", "Junkiion", "Quintessa"],
+    correctAnswer: "Cybertron",
+    image: "<img src='assets/images/cybertronanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 1
+    question: "Finish this song lyric: 'You've got the touch, you've got the ___'",
+    choices: ["Power", "Will", "Strength", "Heart"],
+    correctAnswer: "Power",
+    image: "<img src='assets/images/matrixanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 2
+    question: "Before he became Rodimus Prime, he was known as...?",
+    choices: ["KITT", "Hot Rod", "Mirage", "Air Wolf"],
+    correctAnswer: "Hot Rod",
+    image: "<img src='assets/images/hotrodanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 3
+    question: "Who was the first female Transformer to appear in the cartoon?",
+    choices: ["Elita One", "Firestar", "Arcee", "Nautica"],
+    correctAnswer: "Arcee",
+    image: "<img src='assets/images/arceeanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 4
+    question: "In Beast Wars, Megatron is protrayed as a ___?",
+    choices: ["Stegosaurus", "Triceratops", "Tyrannosaurus Rex", "Velociraptor"],
+    correctAnswer: "Cybertron",
+    image: "<img src='assets/images/megatronnanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 5
+    question: "Scatman Crothers is known for voicing this Transformer?",
+    choices: ["Rumble", "Blurr", "Black Arachnia", "Jazz"],
+    correctAnswer: "Jazz",
+    image: "<img src='assets/images/jazzanswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 6
+    question: "Which of these Transformers is a Combiner?",
+    choices: ["Skytrain", "Devastator", "Scourge", "Beta"],
+    correctAnswer: "Devastator",
+    image: "<img src='assets/images/devastatoranswer.jpg' class='img-circle shadow'>"
+  }, 
+  {
+    // 7
+    question: "Transformers 'borrowed' this character design from the Anime Macross.",
+    choices: ["Ramjet", "Starscream", "Thrust", "Jetfire"],
+    correctAnswer: "Jetfire",
+    image: "<img src='assets/images/jetfireanswer.jpg' class='img-circle shadow'>"
+  }];
+
+// logic that tracks what question in the array we are on, and serves up the next question
+
+// logic for user correct guess
+
+// logic for user incorrect guess
+
+// logic for when user runs out of time
+
+// logic for displaying final result
+
+// logic for game clock
+
+// start the game
+
+// reset the game
+
+// logic that displays the proper screen for right or wrong answer
+
+
+
+    });
