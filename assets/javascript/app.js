705d8f37631a7ac06eba32899ce366040e464bc6
SheriffHobo <zombiemaster@gmail.com>
2019-02-18 18:12:05 -0800

    lots of JS added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,122 @@
 $(function () {
 
-// track which q they are on
-var qCounter = 0;
-// time for each question
-var time = 15;
-// tally correct guesses
+var round = 0;
+var timeLeft = 15;
 var corGuesses = 0;
-// tally incorrect guesses
 var incorGuesses = 0;
-
-// q|a array
+var gameBoard = $('#gamearea');
 var questions = [
   {
-    // 0
     question: "What planet do the Transformers originate?",
     choices: ["Cybertron", "Earth", "Junkiion", "Quintessa"],
     correctAnswer: "Cybertron",
     image: "<img src='assets/images/cybertronanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 1
-    question: "Finish this song lyric: 'You've got the touch, you've got the ___'",
+    question: "Finish this song lyric: 'You've got the touch, you've got the _ '",
     choices: ["Power", "Will", "Strength", "Heart"],
     correctAnswer: "Power",
     image: "<img src='assets/images/matrixanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 2
     question: "Before he became Rodimus Prime, he was known as...?",
     choices: ["KITT", "Hot Rod", "Mirage", "Air Wolf"],
     correctAnswer: "Hot Rod",
     image: "<img src='assets/images/hotrodanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 3
     question: "Who was the first female Transformer to appear in the cartoon?",
     choices: ["Elita One", "Firestar", "Arcee", "Nautica"],
     correctAnswer: "Arcee",
     image: "<img src='assets/images/arceeanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 4
-    question: "In Beast Wars, Megatron is protrayed as a ___?",
+    question: "In Beast Wars, Megatron is protrayed as a _ ?",
     choices: ["Stegosaurus", "Triceratops", "Tyrannosaurus Rex", "Velociraptor"],
-    correctAnswer: "Cybertron",
-    image: "<img src='assets/images/megatronnanswer.jpg' class='img-circle shadow'>"
+    correctAnswer: "Tyrannosaurus Rex",
+    image: "<img src='assets/images/megatronanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 5
     question: "Scatman Crothers is known for voicing this Transformer?",
     choices: ["Rumble", "Blurr", "Black Arachnia", "Jazz"],
     correctAnswer: "Jazz",
     image: "<img src='assets/images/jazzanswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 6
     question: "Which of these Transformers is a Combiner?",
     choices: ["Skytrain", "Devastator", "Scourge", "Beta"],
     correctAnswer: "Devastator",
     image: "<img src='assets/images/devastatoranswer.jpg' class='img-circle shadow'>"
   }, 
   {
-    // 7
-    question: "Transformers 'borrowed' this character design from the Anime Macross.",
+    question: "Transformers 'borrowed' this character design from the Anime 'Macross'.",
     choices: ["Ramjet", "Starscream", "Thrust", "Jetfire"],
     correctAnswer: "Jetfire",
     image: "<img src='assets/images/jetfireanswer.jpg' class='img-circle shadow'>"
   }];
 
-// logic that tracks what question in the array we are on, and serves up the next question
+function playRound() {
+  clearBoard();
+  showQuestion();
+  displayAnswerChoices();
+}
 
-// logic for user correct guess
+function clearBoard() {
+gameBoard.empty();
+};
 
-// logic for user incorrect guess
+function showQuestion() {
+var question = $('<p>').text(questions[round].question);
+gameBoard.append(question);
+};
 
-// logic for when user runs out of time
+function displayAnswerChoices() {
+var answerBox = $('<div>');
+for (let i = 0; i < 4; i++) {
+  var answerChoice = $('<button>');
+  answerChoice.text(questions[round].choices[i]);
+  answerChoice.attr('choice', questions[round].choices[i]);
+  answerBox.append(answerChoice);
+}
+gameBoard.append(answerBox);
+};
 
-// logic for displaying final result
+function evaluateAnswer(answerChoice) {
+if (answerChoice === questions[round].correctAnswer) {
+  displayWinMessage();
+  setTimeout(playRound, 4000);
+  corGuesses++;
+  round++;
+} else {
+  displayWrongMessage();
+  setTimeout(playRound, 4000);
+  incorGuesses++;
+  round++;
+}
+};
 
-// logic for game clock
+function displayWinMessage() {
+  clearBoard();
+  $("#gamearea").html("<p>Excelsior!</p>");
+  gameBoard.append('<p>The correct answer was ' + questions[round].correctAnswer + '</p>' + questions[round].image);
+};
 
-// start the game
+function displayWrongMessage() {
+  clearBoard();
+  $("#gamearea").html("<p>Foolish Human!</p>");
+  gameBoard.append('<p>The correct answer was ' + questions[round].correctAnswer + '!</p>' + questions[round].image);
+};
+
+gameBoard.on('click', 'button', function () {
+  var choice = $(this).attr('choice');
+  evaluateAnswer(choice);
+});
+
+$("#start").click(playRound);
 
 // reset the game
 
 // logic that displays the proper screen for right or wrong answer
 
 
-
-    });
+});
